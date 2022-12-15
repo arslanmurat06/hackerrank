4e6485708e498a589e2efa7a7a295c5c3c6e53fc
arslanmurat06 <mrtarsln06@gmail.com>
2022-12-15 11:51:45 +0300

    jumping clouds added

---
+++ jumping_clouds/Program.cs
@@ -0,0 +1,26 @@
+﻿int jumpingOnClouds(int[] c, int k)
+{
+    int energy_level = 100;
+    var cloudsLength = c.Length;
+    int point = 0;
+
+    while (cloudsLength > point)
+    {
+        point += k;
+
+        if (point >= cloudsLength) point = point - cloudsLength;
+
+        if (c[point] == 1)
+            energy_level -= 2;
+
+        energy_level--;
+
+        if (point == 0) break;
+    }
+    return energy_level;
+
+}
+
+Console.WriteLine(jumpingOnClouds(new int[] { 0, 0, 1, 0, 0, 1, 1, 0 }, 2));
+Console.WriteLine(jumpingOnClouds(new int[] { 0, 0, 1, 0 }, 2));
+Console.WriteLine(jumpingOnClouds(new int[] { 1, 1, 1, 0, 1, 1, 0, 0, 0, 0 }, 3));
