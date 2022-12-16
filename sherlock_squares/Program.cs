e8f59891161d7f26e4dff5abbc3d864f6ec8bb9f
arslanmurat06 <mrtarsln06@gmail.com>
2022-12-16 15:39:25 +0300

    one more added

---
+++ sherlock_squares/Program.cs
@@ -0,0 +1,17 @@
+﻿static int squares(int a, int b)
+{
+    var sqrRootA = (int)Math.Sqrt(a);
+    var sqrRootB = (int)Math.Sqrt(b);
+
+    var isARootable = Math.Ceiling(Math.Sqrt(a)) == Math.Sqrt(a);
+
+
+    return (sqrRootB - sqrRootA) + (isARootable ? 1 : 0);
+
+}
+
+Console.WriteLine(squares(3, 9));//2
+Console.WriteLine(squares(17, 24));//0
+Console.WriteLine(squares(24, 49));//3
+Console.WriteLine(squares(35, 70));//3
+Console.WriteLine(squares(100, 1000));//22
