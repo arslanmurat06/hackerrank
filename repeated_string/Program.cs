b803e133c6e5a1b70544e2aa93bec0f54f82abcf
arslanmurat06 <mrtarsln06@gmail.com>
2022-12-27 21:29:42 +0300

    repeated string

---
+++ repeated_string/Program.cs
@@ -0,0 +1,21 @@
+﻿static long repeatedString(string s, long n)
+{
+
+    var countA = s.Where(c => c == 'a').Count();
+
+    var divide = n / s.Length;
+    var remain = n % s.Length;
+
+    var total = divide * countA;
+
+    for (int i = 0; i < remain; i++)
+    {
+        if (s[i] == 'a')
+            total++;
+    }
+
+    return total;
+}
+
+
+Console.WriteLine(repeatedString("a", 1000000000000));
