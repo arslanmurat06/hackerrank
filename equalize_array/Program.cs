0100d5ce416f77ff02268a7e7ed8e9cbbb88fca9
arslanmurat06 <mrtarsln06@gmail.com>
2023-01-01 00:47:24 +0300

    first solved challenge of hackerrank in 2023

---
+++ equalize_array/Program.cs
@@ -0,0 +1,23 @@
+﻿static int equalizeArray(List<int> arr)
+{
+
+    var numberFreq = new Dictionary<int, int>();
+
+    foreach (var num in arr)
+    {
+        if (!numberFreq.TryGetValue(num, out int count))
+        {
+            numberFreq.Add(num, 1);
+        }
+        else
+            numberFreq[num]++;
+    }
+
+    var maxKey = numberFreq.MaxBy(kvp => kvp.Value).Key;
+    var maxvalue = numberFreq[maxKey];
+
+    return arr.Count - maxvalue;
+}
+
+Console.WriteLine(equalizeArray(new List<int> { 3, 3, 2, 1, 3 }));//2
+Console.WriteLine(equalizeArray(new List<int> { 1, 2, 3, 1, 2, 3, 3, 3 }));//4
